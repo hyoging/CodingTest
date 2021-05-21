c2c85ec47b596bb3549de07e111ae1ccd102dd45
User <w2030@e-mirim.hs.kr>
2021-05-21 09:49:30 +0900

    문제 5번 완성

--- 문제05.py
+++ 문제05.py
@@ -1,15 +1,15 @@
 def solution(number):
    count = 0
-   for i in range(1, number + 1):  # for(int i=1; i<number+1)
+   for i in range(1, number + 1):
        current = i
        temp = count
        while current != 0:
-           if @@@:
+           if current % 10 in [3, 6, 9]:
                count += 1
-               print("pair", end = '') # 디버깅을 위한 출력(없어도 무관)
            current = current // 10
-       if temp == count:
-           print(i, end = '') # 디버깅을 위한 출력(없어도 무관)
-       print(" ", end = '') # 디버깅을 위한 출력(없어도 무관)
-   print("") # 디버깅을 위한 출력(없어도 무관)
    return count
+
+#The following is code to output testcase.
+number = 40
+ret = solution(number)
+print(ret)
