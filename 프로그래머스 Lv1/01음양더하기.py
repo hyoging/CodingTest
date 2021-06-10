445093f72f6c38368be5267e0155243084cdcecb
User <w2030@e-mirim.hs.kr>
2021-06-11 08:59:35 +0900

    Lv1-01음양더하기 시작

---
+++ 프로그래머스 Lv1/01음양더하기.py
@@ -0,0 +1,16 @@
+def solution(absolutes, signs):
+    answer = 123456789
+    return answer
+
+
+a = [4, 7, 12]
+s = [True, False, True]
+print(solution(a, s))
+
+
+    # for i in range(len(absolutes)):
+    #     if signs[i]:
+    #         answer += absolutes[i]
+    #     else:
+    #         answer -= absolutes[i]
+    # return answer
